ea3474769ef330eb68645e5c0309afcb51eb0e5f
fqaisrani <jay.fullstack.0506@gmail.com>
2024-03-24 18:50:51 -0400

    Form

--- app/views/widgets/index.blade.php
+++ app/views/widgets/index.blade.php
@@ -4,6 +4,22 @@
         <title>Widgets</title>
     </head>
     <body>
-        Hello, world!
+
+        {{ Form::open(['url' => '/widgets', 'class' => '']) }}
+            <div>
+                {{ Form::label('name', 'Name') }}
+                {{ Form::text('name', null) }}
+            </div>
+            <div>
+                {{ Form::label('color', 'Color') }}
+                {{ Form::text('color', null) }}
+            </div>
+            <div>
+                {{ Form::label('description', 'Description') }}
+                {{ Form::textarea('description', null) }}
+            </div>
+            {{ Form::submit('Add Widget') }}
+        {{ Form::close() }}
+
     </body>
 </html>
