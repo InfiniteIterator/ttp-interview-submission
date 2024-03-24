2221c562d624cbea2666fccf01b809ea759053e1
fqaisrani <jay.fullstack.0506@gmail.com>
2024-03-24 18:57:43 -0400

    bootstrapify

--- app/views/widgets/index.blade.php
+++ app/views/widgets/index.blade.php
@@ -5,22 +5,24 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     </head>
     <body>
+        <div class="container">
 
-        {{ Form::open(['url' => '/widgets', 'class' => '']) }}
-            <div>
-                {{ Form::label('name', 'Name') }}
-                {{ Form::text('name', null) }}
-            </div>
-            <div>
-                {{ Form::label('color', 'Color') }}
-                {{ Form::text('color', null) }}
-            </div>
-            <div>
-                {{ Form::label('description', 'Description') }}
-                {{ Form::textarea('description', null) }}
-            </div>
-            {{ Form::submit('Add Widget') }}
-        {{ Form::close() }}
+            {{ Form::open(['url' => '/widgets', 'class' => '']) }}
+                <div class="form-group">
+                    {{ Form::label('name', 'Name') }}
+                    {{ Form::text('name', null, ['class' => 'form-control']) }}
+                </div>
+                <div class="form-group">
+                    {{ Form::label('color', 'Color') }}
+                    {{ Form::text('color', null, ['class' => 'form-control']) }}
+                </div>
+                <div class="form-group">
+                    {{ Form::label('description', 'Description') }}
+                    {{ Form::textarea('description', null, ['class' => 'form-control']) }}
+                </div>
+                {{ Form::submit('Add Widget', ['class' => 'btn btn-primary']) }}
+            {{ Form::close() }}
 
+        </div>
     </body>
 </html>
