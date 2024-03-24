295d06dcf906f9420643537341323c49109227b6
fqaisrani <jay.fullstack.0506@gmail.com>
2024-03-24 18:52:34 -0400

    added bootstrap

--- app/views/widgets/index.blade.php
+++ app/views/widgets/index.blade.php
@@ -2,6 +2,7 @@
 
     <head>
         <title>Widgets</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     </head>
     <body>
 
